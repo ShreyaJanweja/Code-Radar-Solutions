5bc39d47e83fc43e5cf752cee7e837c6aad76ec7
Shreya <shreyajanweja26@gmail.com>
2025-02-18 04:25:07 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -1,24 +1,55 @@
-#include <stdio.h>
-int main() {
-    char type;
-    scanf("%c", &type);
-    if(type>='a' && type<='z'||type>='A' && type<='Z'){
-        if(type =='a'||type =='e'||type=='i'||type=='o'||type=='u'){
+// #include <stdio.h>
+// int main() {
+//     char type;
+//     scanf("%c", &type);
+//     if(type>='a' && type<='z'||type>='A' && type<='Z'){
+//         if(type =='a'||type =='e'||type=='i'||type=='o'||type=='u'){
+//             printf("Vowel");
+//         }
+//         else if(type == 'A'||type == 'E'||type == 'I'||type == 'O'||type == 'U'){
+//             printf("Vowel");
+//         }
+//         else{
+//             printf("Consonant");
+//         }
+//     }
+//     else if(type>='0' && type<='9'){
+//         printf("Digit");
+//     }
+//     else{
+//         printf("Special Character");
+//     }
+
+//     return 0;
+// } 
+
+
+#include<stdio.h>
+int main(){
+    char n;
+    scanf("%c", &n);
+    if(n>='a' && n<='z' || n>='A' && n<='Z'){
+        if(n=='a' || n=='e' || n=='i' || n=='o' || n=='u'){
             printf("Vowel");
         }
-        else if(type == 'A'||type == 'E'||type == 'I'||type == 'O'||type == 'U'){
+        else if(n=='A' || n=='E' || n=='I' || n=='O' || n=='U'){
             printf("Vowel");
         }
         else{
             printf("Consonant");
         }
     }
-    else if(type>='0' && type<='9'){
+    else if(n>='0' && n<='9'){
         printf("Digit");
     }
     else{
         printf("Special Character");
     }
-
     return 0;
-} 
+}
+
+
+
+
+
+
