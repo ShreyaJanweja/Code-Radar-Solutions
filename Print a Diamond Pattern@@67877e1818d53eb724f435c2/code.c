7125fc7e4518416747f7fdbad8735cc5ef32e9a4
Shreya <shreyajanweja26@gmail.com>
2025-04-01 01:29:16 +0530

    Solution to Print a Diamond Pattern

--- Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
+++ Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
@@ -1,57 +1,30 @@
-// // Your code here...
-// #include <stdio.h>
-// int main(){
-//     int n;
-//     scanf("%d",&n);
-//     for(int i=1;i<=n;i++){
-//         for(int j=1;j<=n-i;j++){
-//             printf(" ");
-//         }
-//         for(int k=1;k<=2*i-1;k++){
-//             printf("*");
-//         }
-//         printf("\n");
-//     }
-//     for(int i=n-1;i>=1;i--){
-//         for(int j=1;j<=n-i;j++){
-//             printf(" ");
-//         }
-//         for(int k=1;k<=2*i-1;k++){
-//             printf("*");
-//         }
-//         printf("\n");
-//     }
+#include <stdio.h>
 
-
-//     return 0;
-// }
-
-#include<stdio.h>
-int main(){
+int main() {
     int n;
     scanf("%d", &n);
-    for(int i=1; i<=n;i++){
-        for(int j=1; j<= n - i; j++){
+
+    // Upper half (Pyramid)
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n - i; j++) {
             printf(" ");
         }
-        for(int j=1; j <= 2 * i - 1; j++){
+        for(int j = 1; j <= 2 * i - 1; j++) {
             printf("*");
         }
         printf("\n");
     }
-    for(int k = 1; k <= (2 * n - 1); k++){
-        printf("*");
+
+    // Lower half (Inverted Pyramid)
+    for(int i = n - 1; i >= 1; i--) {  // Starts from n-1 to create symmetry
+        for(int j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+        for(int j = 1; j <= 2 * i - 1; j++) {
+            printf("*");
+        }
+        printf("\n");
     }
+
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
