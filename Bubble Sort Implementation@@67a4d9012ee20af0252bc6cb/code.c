5305e74925da890ebd4a7606a33f8978daddc67b
Shreya <shreyajanweja26@gmail.com>
2025-04-07 17:50:46 +0530

    Solution to Bubble Sort Implementation

--- Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -1,51 +1,37 @@
 #include <stdio.h>
 
 // Function to perform Bubble Sort
-void bubbleSort(int *arr, int n) {
-    int i, j, temp;
-    for (i = 0; i < n - 1; i++) {
-        int swapped = 0;
-        for (j = 0; j < n - i - 1; j++) {
-            if (*(arr + j) > *(arr + j + 1)) {
-                temp = *(arr + j);
-                *(arr + j) = *(arr + j + 1);
-                *(arr + j + 1) = temp;
-                swapped = 1;
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
-        if (swapped == 0) break;
     }
 }
 
 // Function to print the array
-void printArray(int *arr, int n) {
+void printArray(int arr[], int n) {
     for (int i = 0; i < n; i++) {
-        printf("%d ", *(arr + i));
+        printf("%d ", arr[i]);
     }
     printf("\n");
 }
 
-// ✅ Only one main function
 int main() {
     int n;
-    // printf("Enter number of elements: ");
     scanf("%d", &n);
-
     int arr[n];
-
-    // printf("Enter %d elements: ", n);
     for (int i = 0; i < n; i++) {
-        scanf("%d", arr + i);
+        scanf("%d", &arr[i]);
     }
 
     bubbleSort(arr, n);
-
-    // printf("Sorted array in ascending order: ");
     printArray(arr, n);
 
     return 0;
 }
-
-
-
-
