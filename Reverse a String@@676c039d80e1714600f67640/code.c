c77602b04e8a6b2505145cf197f990bd59d78ccc
Shreya <shreyajanweja26@gmail.com>
2025-04-11 00:22:35 +0530

    Solution to Reverse a String

--- Reverse a String@@676c039d80e1714600f67640/code.c
+++ Reverse a String@@676c039d80e1714600f67640/code.c
@@ -1,34 +1,49 @@
-// Your code here...
-#include <stdio.h>
-#include <string.h>
+// // Your code here...
+// #include <stdio.h>
+// #include <string.h>
 
-int main() {
-    char str[100];
+// int main() {
+//     char str[100];
 
     
-    fgets(str, sizeof(str), stdin);
+//     fgets(str, sizeof(str), stdin);
 
-    // Remove newline character if fgets captures it
-    size_t len = strlen(str);
-    if (len > 0 && str[len - 1] == '\n') {
-        str[len - 1] = '\0';
-        len--;
+//     // Remove newline character if fgets captures it
+//     size_t len = strlen(str);
+//     if (len > 0 && str[len - 1] == '\n') {
+//         str[len - 1] = '\0';
+//         len--;
+//     }
+
+//     // Reverse the string in place
+//     int start = 0, end = len - 1;
+//     char temp;
+
+//     while (start < end) {
+//         temp = str[start];
+//         str[start] = str[end];
+//         str[end] = temp;
+
+//         start++;
+//         end--;
+//     }
+
+//     printf("%s\n", str);
+
+//     return 0;
+// }
+
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[100];
+    scanf("%s", str);
+
+    int len = strlen(str);
+
+    for(int i= len-1; i>=0; i--){
+        printf("%c", str[i]);
     }
-
-    // Reverse the string in place
-    int start = 0, end = len - 1;
-    char temp;
-
-    while (start < end) {
-        temp = str[start];
-        str[start] = str[end];
-        str[end] = temp;
-
-        start++;
-        end--;
-    }
-
-    printf("%s\n", str);
-
     return 0;
+
 }
